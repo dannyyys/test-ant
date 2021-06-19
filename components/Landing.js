5770bebb2a683db45bb4d7af461a23e89c9b564e
dannyyys <dannyyys@users.noreply.github.com>
2021-06-19 21:31:06 +0800

    feat: :sparkles: Initial commit

---
+++ components/Landing.js
@@ -0,0 +1,16 @@
+import Lottie from "react-lottie-player";
+import { Row } from "antd";
+import landingLottie from "../public/landing_lottie.json";
+
+export default function Landing(props) {
+  return (
+    <div marginTop={props.marginTop}>
+      <Lottie
+        loop
+        animationData={landingLottie}
+        play
+        style={{ width: 350, height: 350 }}
+      />
+    </div>
+  );
+}
