3910e410c82936a25e634efaf335e9e8184d5e4c
dannyyys <dannyyys@users.noreply.github.com>
2021-06-22 15:42:57 +0800

    style: :lipstick: Update font size, margins and button

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { Button, Space, DatePicker, Card, Row, Col, Typography } from "antd";
 import { CiCircleFilled } from "@ant-design/icons";
 import Landing from "../components/Landing";
+import Link from "next/link";
 
 export default function Home() {
   const { Title, Text } = Typography;
@@ -14,19 +15,27 @@ export default function Home() {
       }}
     >
       <Space size="large" direction="vertical" align="center">
-        <Title>TheAroidHouse</Title>
+        <Title level={1} style={{ marginBottom: "10px" }}>
+          TheAroidHouse
+        </Title>
         <Landing />
-        <Text>We try to make gardening simple</Text>
-        <Button
-          type="default"
-          style={{
-            background: "#FFBA00",
-
-            borderRadius: "10px",
-          }}
-        >
-          Primary Button
-        </Button>
+        <Text style={{ fontSize: "24px" }}>
+          We try to make gardening simple
+        </Text>
+        <Link href="/shop" passHref>
+          <Button
+            type="default"
+            style={{
+              background: "#FFBA00",
+              borderRadius: "10px",
+              fontSize: "24px",
+              height: "40px",
+              width: "100px",
+            }}
+          >
+            Shop
+          </Button>
+        </Link>
       </Space>
     </div>
   );
