eede330cc252e378cde42589e446de9be7486a82
dannyyys <dannyyys@users.noreply.github.com>
2021-06-22 16:01:20 +0800

    feat: :sparkles: Add big button

---
+++ components/button/BigButton.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Button } from "antd";
+import { motion } from "framer-motion";
+
+const MotionButton = motion(Button);
+
+export const BigButton = React.forwardRef(function BigButton(props, ref) {
+  return (
+    <span href={props.href} ref={ref}>
+      <MotionButton
+        type="default"
+        style={{
+          background: "#FFBA00",
+          borderRadius: "10px",
+          fontSize: "24px",
+          height: "40px",
+          width: "25vw",
+          outline: "none",
+          color: "#F8FAF8",
+        }}
+        // backgroundColor="buttonBackground"
+        // textColor="background"
+        // fontSize="lg"
+        // width={["50vw", "25vw"]}
+        whileHover={{
+          backgroundColor: "#CC9400",
+          scale: 1.1,
+        }}
+        // padding="7"
+        // borderRadius="5"
+        // whileHover={{ scale: 1.1 }}
+        // fontWeight="bold"
+        {...props}
+      >
+        {props.name}
+      </MotionButton>
+    </span>
+  );
+});
