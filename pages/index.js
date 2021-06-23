84bb585789a822e22b9edfedb92e5e9b92f17f73
dannyyys <dannyyys@users.noreply.github.com>
2021-06-23 20:10:51 +0800

    feat: :sparkles: Add testimonial carousel

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,16 @@ import { CiCircleFilled } from "@ant-design/icons";
 import Landing from "../components/Landing";
 import Link from "next/link";
 import { BigButton } from "../components/button/BigButton";
+import { ImageCarousel } from "../components/item/ImageCarousel";
 
-export default function Home() {
+export default function Home({ testimonialImages }) {
   const { Title, Text } = Typography;
   return (
     <div
       align="center"
       style={{
         background: "#F8FAF8",
-        height: "100vh",
+        height: "auto",
         paddingTop: "15vh",
       }}
     >
@@ -26,7 +27,32 @@ export default function Home() {
         <Link href="/shop" passHref>
           <BigButton name={"Shop now"} />
         </Link>
+        <ImageCarousel images={testimonialImages} />
       </Space>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://api.cosmicjs.com/v2/buckets/danny-testing/objects/60c764902169a900086ca935?pretty=true&read_key=83i3GSGW9cqnXYFv3gp9ijljgUcNIjB7lXEp2JIVYaykZTpuB7&props=metadata,"
+  );
+  const data = await response.json();
+  const cosmicImages = data.object.metadata.images;
+
+  let testimonialImages = [];
+
+  cosmicImages.map((img) => {
+    testimonialImages.push(img.image.imgix_url);
+  });
+  console.log(testimonialImages);
+
+  return {
+    props: {
+      testimonialImages,
+    },
+    // incrementally regenerate every 2 hours
+    // seconds * minutes * hours
+    revalidate: 60 * 60 * 2,
+  };
 }
